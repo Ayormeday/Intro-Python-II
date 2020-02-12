64e6dd6f35c0cfbbfbd44d62c2f94476aa2827bb
Ayomide Jones <ayormeday@gmail.com>
2020-02-12 23:48:48 +0100

    player class initialized

--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    #constructor
+    def __init__(self, name, information = []):
+        #attribute(name, room)
+        self.name = name
+        self.information = information
