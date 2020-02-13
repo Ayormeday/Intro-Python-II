38fa301c854fc94ed884a65a52f0a7d2a71c6d09
Ayomide Jones <ayormeday@gmail.com>
2020-02-13 01:29:17 +0100

    added logic to get items in rooms

--- src/room.py
+++ src/room.py
@@ -6,4 +6,12 @@ class Room:
         self.name = name
         self.description = description
         self.items = items
-    
+
+    def fetchItem(self, itemlist, qty=[]):
+        for item in self.items:
+            if itemlist.name not in [item.name]:
+                self.items.append(item(itemlist.name, itemlist.description, qty))
+        else: 
+            for item in self.items:
+                if item.name == itemlist.name:
+                    item.qty += qty
